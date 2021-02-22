bc213ce8f0fbd57c7a0d93687da3695dc85a8ffa
BPM <brantleymorris@me.com>
2021-02-22 15:20:46 -0600

    initial commit, it is really long due to lack of internet last week

---
+++ models/exercise.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercise: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Exercise type is required."
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Exercise name is required."
+        },
+        distance: {
+            type: Number
+        },
+        duration: {
+            type: Number,
+            required: "Exercise durations is required."
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        }
+    }]
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
